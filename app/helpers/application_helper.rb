522f0e35b8caa84eca305469a79f35fc84828381
bcolcolough <bcolcolough@gmail.com>
2015-06-23 00:41:49 +0200

    added logo and bootstrap styling to welcome page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
 	def current_action?(controller_name, action_name)
 		controller.controller_name == controller_name && controller.action_name == action_name
 	end
+
+	def resource_name
+    	:user
+  	end
+
+  	def resource
+    	@resource ||= User.new
+  	end
+
+  	def devise_mapping
+    	@devise_mapping ||= Devise.mappings[:user]
+  	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#show'
+  root 'welcome#index'
 
   resources :users, :only => [:show, :index, :edit]
 
